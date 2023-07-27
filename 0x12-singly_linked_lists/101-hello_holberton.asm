b537db1e854b6bfebe7b75c6be4705f226d18cbd
Chaimaeelalaoui <elalaouichaimaee@gmail.com>
2023-07-27 05:24:12 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,10 @@
-ection   .text
-	global main
+	global   main
+	  extern    printf
 main:
-	mov       rax, 1
-	mov       rdi, 1
-	mov       rsi, msg
-	mov       rdx, 17
-	syscall
-	mov       rax, 60
-	xor       rdi, rdi
-	syscall
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov   eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
 
-	section   .data
-msg:	db        "Hello, Holberton", 10
